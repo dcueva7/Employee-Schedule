17bc3e75fcf17612105b6903ab08db107023b6d1
Daniel Cueva <dcueva@usc.edu>
2023-05-23 13:36:49 -0700

    lifted up the fetch shifts function from Schedule

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Dashboard from './Dashboard';
 import EmployeeShiftContext from './EmployeeShiftContext';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+
+import useAuth from './UseAuth'
 
 import {
   BrowserRouter as Router,
@@ -22,9 +24,47 @@ function App() {
   const [ employees, setEmployees ] = useState([])
   const [ adjustments, setAdjustments ] = useState([])
 
+  const authToken = useAuth()
+
+  const fetchShift = useCallback(() => {
+    fetch('/shift/list_shifts/', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${authToken}`,
+        },
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json,'fetched all shifts')
+            const data = json.map(item => ({
+                id: item.id,
+                title : `${item.student.first_name} ${item.student.last_name}`,
+                start : `${item.date}T${item.start_time}`,
+                end : `${item.date}T${item.end_time}`,
+                color : item.student.color,
+                extendedProps : {
+                    student_id : item.student.user,
+                    employee_id : item.student.id,
+                },
+            }));
+            setShifts(data)
+            
+        }).catch(error => {
+            console.error('Error:', error);
+        })
+  }, [setShifts, authToken])
+
   return (
     <ChakraProvider>
-      <EmployeeShiftContext.Provider value={{shifts, setShifts, employees, setEmployees, adjustments, setAdjustments}} >
+      <EmployeeShiftContext.Provider value={{
+        shifts, 
+        setShifts, 
+        employees, 
+        setEmployees, 
+        adjustments, 
+        setAdjustments,
+        fetchShift
+      }} >
         <Router>
           <Routes>
               <Route path="/" exact element={<Schedule />} />
