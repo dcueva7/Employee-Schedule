54676d86cdff1e5a3377be65ae46ec96f8c9d7cc
Daniel Cueva <dcueva@usc.edu>
2023-05-23 21:36:43 -0700

    converted createRecurring Schedule function to async funciton so i can chain with .then

--- frontend/src/useRecurringSchedule.js
+++ frontend/src/useRecurringSchedule.js
@@ -8,18 +8,21 @@ const useRecurringSchedule = () => {
 
     const authToken = Cookies.get('authToken')
 
-    const createRecurringSchedule = () => {
-
-        fetch('create_recurring_schedule/', {
-            method : 'GET',
-            headers : {
-                'Content-type' : 'application/json',
-                'Authorization' : `Token ${authToken}`,
-            }
-        })
-            .then(response => response.json())
-            .then(json => console.log(json))
-            .catch(error => console.error(error))
+    const createRecurringSchedule = async () => {
+        try {
+            const response = await fetch('create_recurring_schedule/', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': `Token ${authToken}`,
+                }
+            });
+            const json = await response.json();
+            console.log(json);
+            
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return { createRecurringSchedule }
