58e6911362aeab5e272b5b3f2c00a6d605fc4508
Daniel Cueva <dcueva@usc.edu>
2023-06-09 00:07:47 -0700

    configured connection to AWS RDS database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'den',
+        'USER': 'admin',
+        'PASSWORD': os.getenv("DATABASE_PASS"),
+        'HOST': os.getenv("DATABASE_ENDPOINT"),
+        'PORT': '3306',  
     }
 }
 
